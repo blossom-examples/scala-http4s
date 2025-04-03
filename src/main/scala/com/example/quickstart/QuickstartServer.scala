d3804868aadc6253dc366b1ecf7f4bee4efff2b8
Tung Nguyen <tongueroo@gmail.com>
2025-04-03 18:57:34 +0000

    add home route

--- src/main/scala/com/example/quickstart/QuickstartServer.scala
+++ src/main/scala/com/example/quickstart/QuickstartServer.scala
@@ -8,20 +8,32 @@ import org.http4s.ember.client.EmberClientBuilder
 import org.http4s.ember.server.EmberServerBuilder
 import org.http4s.implicits.*
 import org.http4s.server.middleware.Logger
+import org.http4s.dsl.Http4sDsl
+import org.http4s.HttpRoutes
 
 object QuickstartServer:
 
   def run[F[_]: Async: Network]: F[Nothing] = {
+    val dsl = new Http4sDsl[F]{}
+    import dsl.*
+
     for {
       client <- EmberClientBuilder.default[F].build
       helloWorldAlg = HelloWorld.impl[F]
       jokeAlg = Jokes.impl[F](client)
 
-      // Combine Service Routes into an HttpApp.
-      // Can also be done via a Router if you
-      // want to extract segments not checked
-      // in the underlying routes.
+      // Create home route that uses HelloWorld service
+      homeRoute = HttpRoutes.of[F] {
+        case GET -> Root =>
+          for {
+            greeting <- helloWorldAlg.hello(HelloWorld.Name("World"))
+            resp <- Ok(greeting)
+          } yield resp
+      }
+
+      // Combine Service Routes into an HttpApp
       httpApp = (
+        homeRoute <+>
         QuickstartRoutes.helloWorldRoutes[F](helloWorldAlg) <+>
         QuickstartRoutes.jokeRoutes[F](jokeAlg)
       ).orNotFound
@@ -29,7 +41,7 @@ object QuickstartServer:
       // With Middlewares in place
       finalHttpApp = Logger.httpApp(true, true)(httpApp)
 
-      _ <- 
+      _ <-
         EmberServerBuilder.default[F]
           .withHost(ipv4"0.0.0.0")
           .withPort(port"8080")
